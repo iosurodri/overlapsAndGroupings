f175942df99570fa085d7ec7bd2861e224da6429
iosurodri <iosu-rodri@hotmail.com>
2021-09-16 17:31:28 +0200

    REFACT: Adapted to the used models

--- src/runs/run_evaluation.py
+++ src/runs/run_evaluation.py
@@ -13,12 +13,11 @@ from src.model_tools.load_model import load_model
 import torch
 
 
-def run_evaluation(model_file_name, model_type='dense', info_file_name=None, dataset='COVID'):
+def run_evaluation(model_file_name, model_type='lenet', info_file_name=None, dataset='CIFAR10'):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     batch_size = 64
-    test_loader = load_dataset(dataset, batch_size=batch_size, train=False, num_workers=0, pin_memory=True,
-                               colour=True)
+    test_loader = load_dataset(dataset, batch_size=batch_size, train=False, num_workers=0, pin_memory=True)
     model = load_model(model_file_name, model_type=model_type, info_file_name=info_file_name).to(device)
     prediction_metrics = get_prediction_metrics(model, device=device, test_loader=test_loader)
     print(prediction_metrics)
@@ -26,11 +25,11 @@ def run_evaluation(model_file_name, model_type='dense', info_file_name=None, dat
 
 if __name__ == '__main__':
     # model_file_name = 'COVID4_2layerclassifier__lr0001__partitioned__adam_checkpoint'
-    base_folder = 'dense_COVID3'
-    model_file_name = 'dense_COVID3_pool_sugeno_general_weight_clases_checkpoint'
+    base_folder = ''
+    model_file_name = 'lenet_product'
     model_file_name = os.path.join(base_folder, model_file_name)
-    model_type = 'dense'
-    info_file_name = 'dense_COVID3_pool_sugeno_general_weight_clases_info.json'
+    model_type = 'lenet'
+    info_file_name = model_file_name + '_info.json'
     info_file_name = os.path.join(base_folder, info_file_name)
-    dataset = 'COVID3'
-    run_evaluation(model_file_name, model_type='dense', info_file_name=info_file_name, dataset=dataset)
+    dataset = 'CIFAR10'
+    run_evaluation(model_file_name, model_type=model_type, info_file_name=info_file_name, dataset=dataset)
