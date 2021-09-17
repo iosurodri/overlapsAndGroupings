44a83bcef2ee058ebd529e3333c571da46552b5c
iosurodri <iosu-rodri@hotmail.com>
2021-09-17 14:13:24 +0200

    FIX: SupervisedCrossEntropyLoss now acts as CrossEntropyLoss when epoch=None

--- src/functions/loss_functions.py
+++ src/functions/loss_functions.py
@@ -9,9 +9,13 @@ class SupervisedCrossEntropyLoss(nn.Module):
         self.num_epochs = num_epochs
         self.alpha = alpha
 
-    def forward(self, inputs: torch.Tensor, target: torch.Tensor, epoch):
-        input_final, inputs_supervised = inputs
-        loss = self.crossEntropyLoss(input_final, target)
-        for input_supervised in inputs_supervised:
-            loss += self.crossEntropyLoss(input_supervised, target) * (self.alpha * 0.1 * (1 - float(epoch / self.num_epochs)))
+    def forward(self, inputs: torch.Tensor, target: torch.Tensor, epoch=None):
+        if epoch is None:
+            # If epoch is None, we assume that we are in an evaluation loop, so we return the classic CrossEntropy loss value.
+            loss = self.crossEntropyLoss(inputs, target)
+        else:
+            input_final, inputs_supervised = inputs
+            loss = self.crossEntropyLoss(input_final, target)
+            for input_supervised in inputs_supervised:
+                loss += self.crossEntropyLoss(input_supervised, target) * (self.alpha * 0.1 * (1 - float(epoch / self.num_epochs)))
         return loss.contiguous()
