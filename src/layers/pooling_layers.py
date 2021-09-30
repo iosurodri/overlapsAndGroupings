d186ff637a930fb69fd8505815499c3e857fc539
iosurodri <iosu-rodri@hotmail.com>
2021-09-30 13:24:55 +0200

    FIX: kernel_size and stride are now always treated as 2d lists
    Also, removed automorphism logic from GroupingPlusPool2d

--- src/layers/pooling_layers.py
+++ src/layers/pooling_layers.py
@@ -37,7 +37,11 @@ class OverlapPool2d(torch.nn.Module):
 
     def __init__(self, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, overlap=None, normalization=None, denormalize=False):
         super().__init__()
+        if type(kernel_size) == int:
+            kernel_size = (kernel_size, kernel_size)
         self.kernel_size = kernel_size
+        if type(stride) == int:
+            stride = (stride, stride)
         self.stride = stride if (stride is not None) else kernel_size
         self.padding = padding
         self.dilation = dilation
@@ -59,10 +63,10 @@ class OverlapPool2d(torch.nn.Module):
         if isinstance(self.padding, list) or isinstance(self.padding, tuple):
             tensor = F.pad(tensor, self.padding)
         # 1.-Extract patches of kernel_size from tensor:
-        tensor = tensor.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
+        tensor = tensor.unfold(2, self.kernel_size[0], self.stride[0]).unfold(3, self.kernel_size[1], self.stride[1])
         # 2.-Turn each one of those 2D patches into a 1D vector:
         tensor = tensor.reshape((tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3],
-                                 self.kernel_size * self.kernel_size))
+                                 self.kernel_size[0] * self.kernel_size[1]))
         # 3.-ToDo: Normalize the input so that overlaps defined in (0, 1) can be properly applied:
         output_tensor, normalization_params = self.normalization(tensor)
         # output_tensor = tensor
@@ -109,10 +113,9 @@ class GroupingPool2d(torch.nn.Module):
         if type(kernel_size) == int:
             kernel_size = (kernel_size, kernel_size)
         self.kernel_size = kernel_size
-        stride = stride if (stride is not None) else kernel_size
         if type(stride) == int:
             stride = (stride, stride)
-        self.stride = stride
+        self.stride = stride if (stride is not None) else kernel_size
         self.padding = padding
         self.dilation = dilation
         self.ceil_mode = ceil_mode
@@ -165,7 +168,11 @@ class GroupingPlusPool2d(torch.nn.Module):
     def __init__(self, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping=None, normalization=None, denormalize=True, weight_mode='single',
                  num_channels=1):
         super().__init__()
+        if type(kernel_size) == int:
+            kernel_size = (kernel_size, kernel_size)
         self.kernel_size = kernel_size
+        if type(stride) == int:
+            stride = (stride, stride)
         self.stride = stride if (stride is not None) else kernel_size
         self.padding = padding
         self.dilation = dilation
@@ -193,102 +200,22 @@ class GroupingPlusPool2d(torch.nn.Module):
         if isinstance(self.padding, list) or isinstance(self.padding, tuple):
             tensor = F.pad(tensor, self.padding)
         # 1.-Extract patches of kernel_size from tensor:
-        tensor = tensor.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
+        tensor = tensor.unfold(2, self.kernel_size[0], self.stride[0]).unfold(3, self.kernel_size[1], self.stride[1])
         # 2.-Turn each one of those 2D patches into a 1D vector:
         tensor = tensor.reshape((tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3],
-                                 self.kernel_size * self.kernel_size))
+                                 self.kernel_size[0] * self.kernel_size[1]))
         # 3.-ToDo: Normalize the input so that groupings defined in (0, 1) can be properly applied:
         output_tensor, normalization_params = self.normalization(tensor)
         # If automorphisms are to be used, apply them before applying the grouping (if chosen to do so):
         
         # 4.-Compute reduction based on the chosen grouping:
-        output_tensor = self.grouping(output_tensor, dim=-1)
+        output_tensor = self.grouping(output_tensor, self.weight, dim=-1)
 
         # 5.-Denormalize output values after applying grouping
         if self.denormalize:
             output_tensor = self.denormalization(output_tensor, normalization_params)
-        
-        if self.transform_before and self.automorphism_weight is not None:
-            output_tensor = torch.pow(output_tensor+0.000001, self.automorphism_weight) 
-        # 4.-Compute reduction based on the chosen grouping:
-        output_tensor = self.grouping(output_tensor, dim=-1)
-        # If automorphisms are to be used, apply them after applying the grouping (if chosen to do so):
-        if not self.transform_before and self.automorphism_weight is not None:
-            output_tensor = torch.pow(output_tensor+0.000001, self.automorphism_weight[:, 0])
-        
 
         return output_tensor
-
-
-
-# class GroupingPlusPool2d(torch.nn.Module):
-
-#     available_groupings = {
-#         'product': aggr_funcs.product_grouping,
-#         'maximum': lambda x: torch.max(x)[0],
-#         'minimum': aggr_funcs.minimum_grouping,
-#         'ob': aggr_funcs.ob_grouping,
-#         'geometric': aggr_funcs.geometric_grouping
-#     }
-
-#     available_normalizations = {
-#         'min_max': {
-#             'normalization': aux_funcs.min_max_normalization,
-#             'denormalization': aux_funcs.min_max_denormalization
-#         }
-#     }
-
-#     def __init__(self, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping=None, normalization=None, denormalize=True, automorphism_type='same', transform_before=True):
-#         super().__init__()
-#         self.kernel_size = kernel_size
-#         self.stride = stride if (stride is not None) else kernel_size
-#         self.padding = padding
-#         self.dilation = dilation
-#         self.ceil_mode = ceil_mode
-#         if grouping not in self.available_groupings.keys():
-#             raise Exception('Grouping {} unavailable for GroupingPlusPool2d. Must be one of {}'.format(
-#                 grouping, self.available_groupings.keys()))
-#         self.grouping = self.available_groupings[grouping]
-#         if normalization not in self.available_normalizations.keys():
-#             raise Exception('Normalization {} unavailable for GroupingPlusPool2d. Must be one of {}'.format(
-#                 normalization, self.available_normalizations.keys()))
-#         self.normalization = self.available_normalizations[normalization]['normalization']
-#         self.denormalize = denormalize
-#         self.denormalization = self.available_normalizations[normalization]['denormalization']
-#         # Parameters which model the automorphishms to apply to the 
-#         if automorphism_type == 'same':
-#             # self.automorphism_weight = torch.nn.Parameter(torch.rand(1, 1, 1, 1, 1))
-#             self.automorphism_weight = torch.nn.Parameter(torch.ones([1, 1, 1, 1, 1]) * 0.5)
-#         elif automorphism_type == 'distinct':
-#             self.automorphism_weight = torch.nn.parameter(torch.rand(1, 1, 1, 1, kernel_size*kernel_size))
-#         else:
-#             raise Exception('Unavailable automorphism_type. Must be either "same" or "disctinct"')
-#         self.transform_before = transform_before
-        
-
-#     def forward(self, tensor):
-#         if isinstance(self.padding, list) or isinstance(self.padding, tuple):
-#             tensor = F.pad(tensor, self.padding)
-#         # 1.-Extract patches of kernel_size from tensor:
-#         tensor = tensor.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
-#         # 2.-Turn each one of those 2D patches into a 1D vector:
-#         tensor = tensor.reshape((tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3],
-#                                  self.kernel_size * self.kernel_size))
-#         # 3.-ToDo: Normalize the input so that groupings defined in (0, 1) can be properly applied:
-#         output_tensor, normalization_params = self.normalization(tensor)
-#         # If automorphisms are to be used, apply them before applying the grouping (if chosen to do so):
-#         if self.transform_before and self.automorphism_weight is not None:
-#             output_tensor = torch.pow(output_tensor+0.000001, self.automorphism_weight) 
-#         # 4.-Compute reduction based on the chosen grouping:
-#         output_tensor = self.grouping(output_tensor, dim=-1)
-#         # If automorphisms are to be used, apply them after applying the grouping (if chosen to do so):
-#         if not self.transform_before and self.automorphism_weight is not None:
-#             output_tensor = torch.pow(output_tensor+0.000001, self.automorphism_weight[:, 0])
-#         # 5.-Denormalize output values after applying grouping
-#         if self.denormalize:
-#             output_tensor = self.denormalization(output_tensor, normalization_params)
-
-#         return output_tensor
 
 
 def pickPoolLayer(pool_option):
