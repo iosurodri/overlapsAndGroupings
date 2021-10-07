f891f594cfe01b5d4a261cfac5148108105f109d
iosurodri <iosu-rodri@hotmail.com>
2021-10-07 16:29:02 +0200

    REFACT: Renamed forward input and corrected mispelling in "self.block_2_superivision"

--- src/models/SupervisedNiNPlus.py
+++ src/models/SupervisedNiNPlus.py
@@ -1,5 +1,8 @@
 import torch.nn as nn
 
+# Debug:
+import torch
+
 from math import ceil, floor
 
 
@@ -82,7 +85,7 @@ class SupervisedNiNPlus(nn.Module):
         self.block_2_conv1 = nn.Conv2d(self.network_params['mlpconv_neurons'][0], self.network_params['conv_filters'][2],
                                        kernel_size=3, stride=1, padding=1)
         self.relu4 = nn.ReLU(inplace=True)
-        self.block_2_superivision1 = HiddenLayerSupervision(
+        self.block_2_supervision1 = HiddenLayerSupervision(
             (input_size[0]//2) * (input_size[1]//2) * self.network_params['conv_filters'][2], num_classes,
             classifier_name=supervision_type
         )
@@ -125,11 +128,11 @@ class SupervisedNiNPlus(nn.Module):
             # having to set the size of the output up at to this point (which may vary depending on the used dataset).
 
 
-    def forward(self, x):
+    def forward(self, input):
         if self.training:
             x_supervised = []
         # Block 1:
-        x = self.block_1_conv1(x)
+        x = self.block_1_conv1(input)
         x = self.relu1(x)
         if self.training:
             x_supervised.append(self.block_1_supervision1(x))
@@ -145,7 +148,7 @@ class SupervisedNiNPlus(nn.Module):
         x = self.block_2_conv1(x)
         x = self.relu4(x)
         if self.training:
-            x_supervised.append(self.block_2_superivision1(x))
+            x_supervised.append(self.block_2_supervision1(x))
         x = self.block_2_conv2(x)
         x = self.relu5(x)
         if self.training:
@@ -168,6 +171,15 @@ class SupervisedNiNPlus(nn.Module):
         x = self.avg_pool(x)
 
         x = x.reshape([x.shape[0], x.shape[1]])
+
+        # # Debug:
+        # some_nan = torch.sum(torch.isnan(x)) > 0
+        # if self.training:
+        #     for x_superv in x_supervised:
+        #         if torch.sum(torch.isnan(x_superv)) > 0:
+        #             some_nan = True
+        # if some_nan:
+        #     print('Error')
 
         if self.training:
             return (x, x_supervised)
