03494f81c94164a141dae3141fa9565e96d979a1
iosurodri <iosu-rodri@hotmail.com>
2021-10-25 15:51:02 +0200

    NEW: Added GroupingCompositionPool2d and GroupingCombPool2d

--- src/layers/pooling_layers.py
+++ src/layers/pooling_layers.py
@@ -219,12 +219,178 @@ class GroupingPlusPool2d(torch.nn.Module):
 
         return output_tensor
 
+class GroupingCompositionPool2d(torch.nn.Module):
+
+    available_groupings = {
+        'product': aggr_funcs.product_grouping,
+        'minimum': aggr_funcs.minimum_grouping,
+        'maximum': lambda x, dim=-1: torch.max(x, dim=dim)[0],
+        'maximum_sqrt': lambda x: torch.pow(torch.max(x)[0] + 0.000001, 0.5),
+        'maximum_square': lambda x: torch.pow(torch.max(x)[0] + 0.000001, 2),
+        'ob': aggr_funcs.ob_grouping,
+        'geometric': aggr_funcs.geometric_grouping,
+        'u': aggr_funcs.u_grouping
+    }
+
+    available_normalizations = {
+        'min_max': {
+            'normalization': aux_funcs.min_max_normalization,
+            'denormalization': aux_funcs.min_max_denormalization
+        },
+        'quantile': {
+            'normalization': aux_funcs.quantile_normalization,
+            'denormalization': aux_funcs.min_max_denormalization
+        },
+        'sigmoid': {
+            'normalization': lambda x: (torch.sigmoid(x), None),
+            'denormalization': aux_funcs.sigmoid_denormalization
+        }
+    }
+
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping_big='max', grouping_list=None, normalization=None, denormalize=True):
+        super().__init__()
+        if type(kernel_size) == int:
+            kernel_size = (kernel_size, kernel_size)
+        self.kernel_size = kernel_size
+        if type(stride) == int:
+            stride = (stride, stride)
+        self.stride = stride if (stride is not None) else kernel_size
+        self.padding = padding
+        self.dilation = dilation
+        self.ceil_mode = ceil_mode
+        if grouping_big not in self.available_groupings.keys():
+            raise Exception('Grouping {} unavailable. Must be one of {}'.format(
+                grouping_big, self.available_groupings.keys()))
+        for grouping in grouping_list:
+            if grouping not in self.available_groupings.keys():
+                raise Exception('Grouping {} unavailable. Must be one of {}'.format(
+                    grouping, self.available_groupings.keys()))        
+        self.grouping_big = self.available_groupings[grouping_big]
+        self.grouping_list = [self.available_groupings[grouping] for grouping in grouping_list]
+        if normalization not in self.available_normalizations.keys():
+            raise Exception('Normalization {} unavailable for GroupingPool2d. Must be one of {}'.format(
+                normalization, self.available_normalizations.keys()))
+        self.normalization = self.available_normalizations[normalization]['normalization']
+        self.denormalize = denormalize
+        self.denormalization = self.available_normalizations[normalization]['denormalization']
+
+    def forward(self, tensor):
+        if isinstance(self.padding, list) or isinstance(self.padding, tuple):
+            tensor = F.pad(tensor, self.padding)
+        # 1.-Extract patches of kernel_size from tensor:
+        tensor = tensor.unfold(2, self.kernel_size[0], self.stride[0]).unfold(3, self.kernel_size[1], self.stride[1])
+        # 2.-Turn each one of those 2D patches into a 1D vector:
+        tensor = tensor.reshape((tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3],
+                                 self.kernel_size[0] * self.kernel_size[1]))
+        # 3.-ToDo: Normalize the input so that groupings defined in (0, 1) can be properly applied:
+        normalized_tensor, normalization_params = self.normalization(tensor)
+        # 4.-Compute reduction based on the chosen grouping:
+
+        # Generate an auxiliar tensor the size of normalized_tensor, with as many values for the last dimension as groupings to apply
+        output_tensor = tensor.new_zeros([*tensor.shape[:-1], len(self.grouping_list)])
+        for idx, grouping in enumerate(self.grouping_list):
+            output_tensor[..., idx] = grouping(normalized_tensor, dim=-1) 
+
+        # Aggregate all the obtained outputs (values on the last dimension) by means of self.grouping_big
+        output_tensor = self.grouping_big(output_tensor, dim=-1)
+
+        # 5.-Denormalize output values after applying grouping
+        if self.denormalize:
+            output_tensor = self.denormalization(output_tensor, normalization_params)
+
+        return output_tensor
+
+
+class GroupingCombPool2d(torch.nn.Module):
+
+    available_groupings = {
+        'product': aggr_funcs.product_grouping,
+        'minimum': aggr_funcs.minimum_grouping,
+        'maximum': lambda x, dim=-1: torch.max(x, dim=dim)[0],
+        'maximum_sqrt': lambda x: torch.pow(torch.max(x)[0] + 0.000001, 0.5),
+        'maximum_square': lambda x: torch.pow(torch.max(x)[0] + 0.000001, 2),
+        'ob': aggr_funcs.ob_grouping,
+        'geometric': aggr_funcs.geometric_grouping,
+        'u': aggr_funcs.u_grouping
+    }
+
+    available_normalizations = {
+        'min_max': {
+            'normalization': aux_funcs.min_max_normalization,
+            'denormalization': aux_funcs.min_max_denormalization
+        },
+        'quantile': {
+            'normalization': aux_funcs.quantile_normalization,
+            'denormalization': aux_funcs.min_max_denormalization
+        },
+        'sigmoid': {
+            'normalization': lambda x: (torch.sigmoid(x), None),
+            'denormalization': aux_funcs.sigmoid_denormalization
+        }
+    }
+
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping_list=None, normalization=None, denormalize=True, learnable=True):
+        super().__init__()
+        if type(kernel_size) == int:
+            kernel_size = (kernel_size, kernel_size)
+        self.kernel_size = kernel_size
+        if type(stride) == int:
+            stride = (stride, stride)
+        self.stride = stride if (stride is not None) else kernel_size
+        self.padding = padding
+        self.dilation = dilation
+        self.ceil_mode = ceil_mode
+        for grouping in grouping_list:
+            if grouping not in self.available_groupings.keys():
+                raise Exception('Grouping {} unavailable. Must be one of {}'.format(
+                    grouping, self.available_groupings.keys()))        
+        self.grouping_list = [self.available_groupings[grouping] for grouping in grouping_list]
+        if normalization not in self.available_normalizations.keys():
+            raise Exception('Normalization {} unavailable for GroupingPool2d. Must be one of {}'.format(
+                normalization, self.available_normalizations.keys()))
+        self.normalization = self.available_normalizations[normalization]['normalization']
+        self.denormalize = denormalize
+        self.denormalization = self.available_normalizations[normalization]['denormalization']
+        self.weight = None
+        if learnable:
+            self.weight = torch.nn.Parameter(torch.ones([len(self.grouping_list)]) * (1 / len(self.grouping_list)))
+
+    def forward(self, tensor):
+        if isinstance(self.padding, list) or isinstance(self.padding, tuple):
+            tensor = F.pad(tensor, self.padding)
+        # 1.-Extract patches of kernel_size from tensor:
+        tensor = tensor.unfold(2, self.kernel_size[0], self.stride[0]).unfold(3, self.kernel_size[1], self.stride[1])
+        # 2.-Turn each one of those 2D patches into a 1D vector:
+        tensor = tensor.reshape((tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3],
+                                 self.kernel_size[0] * self.kernel_size[1]))
+        # 3.-ToDo: Normalize the input so that groupings defined in (0, 1) can be properly applied:
+        normalized_tensor, normalization_params = self.normalization(tensor)
+
+        if self.weight is None:
+            weight = [1 / len(self.grouping_list) for i in range(len(self.grouping_list))]
+        else:
+            weight = torch.softmax(self.weight, dim=-1)
+
+        # 4.-Compute reduction based on the chosen grouping:
+        # Generate an auxiliar tensor the size of normalized_tensor, with as many values for the last dimension as groupings to apply
+        output_tensor = tensor.new_zeros(tensor.shape[:-1])
+        for idx, grouping in enumerate(self.grouping_list):
+            output_tensor += weight[idx] * grouping(normalized_tensor, dim=-1) 
+
+        # 5.-Denormalize output values after applying grouping
+        if self.denormalize:
+            output_tensor = self.denormalization(output_tensor, normalization_params)
+
+        return output_tensor
+
 
 def pickPoolLayer(pool_option, initial_pool_exp=None):
     
     defaultOverlap2d = lambda kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, overlap=None, normalization='min_max', denormalize=True: OverlapPool2d(kernel_size, stride, padding, dilation, ceil_mode, overlap, normalization, denormalize)
     defaultGrouping2d = lambda kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping=None, normalization='min_max', denormalize=True: GroupingPool2d(kernel_size, stride, padding, dilation, ceil_mode, grouping, normalization, denormalize)
     defaultGroupingPlus2d = lambda kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping=None, normalization='min_max', denormalize=True, initial_pool_exp=None: GroupingPlusPool2d(kernel_size, stride, padding, dilation, ceil_mode, grouping, normalization, denormalize, initial_pool_exp=initial_pool_exp)
+    defaultGroupingComposition2d = lambda kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping_big=None, grouping_list=None, normalization='min_max', denormalize=True: GroupingCompositionPool2d(kernel_size, stride, padding, dilation, ceil_mode, grouping_big, grouping_list, normalization, denormalize)
+    defaultGroupingComb2d = lambda kernel_size, stride=None, padding=0, dilation=1, ceil_mode=False, grouping_list=None, normalization='min_max', denormalize=True, learnable=False: GroupingCombPool2d(kernel_size, stride, padding, dilation, ceil_mode, grouping_list, normalization, denormalize, learnable)
 
     available_options = {
         'max': torch.nn.MaxPool2d,
@@ -243,10 +409,21 @@ def pickPoolLayer(pool_option, initial_pool_exp=None):
         'grouping_plus_product': lambda kernel_size, stride=None, padding=0, grouping='product_power', initial_pool_exp=initial_pool_exp: defaultGroupingPlus2d(kernel_size, stride, padding, grouping=grouping, initial_pool_exp=initial_pool_exp),
         'grouping_plus_geometric': lambda kernel_size, stride=None, padding=0, grouping='geometric_power': defaultGroupingPlus2d(kernel_size, stride, padding, grouping=grouping),
 
-        'overlap_product': lambda kernel_size, stride=None, padding=0, overlap='product': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
-        'overlap_minimum': lambda kernel_size, stride=None, padding=0, overlap='minimum': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
-        'overlap_ob': lambda kernel_size, stride=None, padding=0, overlap='ob': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
-        'overlap_geometric': lambda kernel_size, stride=None, padding=0, overlap='geometric': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap)
+        'grouping_comp_max_prodAndOB': lambda kernel_size, stride=None, padding=0, grouping_big='maximum', grouping_list=['product', 'ob']: defaultGroupingComposition2d(kernel_size, stride, padding, grouping_big=grouping_big, grouping_list=grouping_list),
+        'grouping_comp_prod_maxAndProd': lambda kernel_size, stride=None, padding=0, grouping_big='product', grouping_list=['maximum', 'product']: defaultGroupingComposition2d(kernel_size, stride, padding, grouping_big=grouping_big, grouping_list=grouping_list),
+        'grouping_comp_prod_maxAndOB': lambda kernel_size, stride=None, padding=0, grouping_big='product', grouping_list=['maximum', 'ob']: defaultGroupingComposition2d(kernel_size, stride, padding, grouping_big=grouping_big, grouping_list=grouping_list),
+        'grouping_comp_prod_prodAndOB': lambda kernel_size, stride=None, padding=0, grouping_big='product', grouping_list=['product', 'ob']: defaultGroupingComposition2d(kernel_size, stride, padding, grouping_big=grouping_big, grouping_list=grouping_list),
+        'grouping_comp_ob_maxAndProd': lambda kernel_size, stride=None, padding=0, grouping_big='ob', grouping_list=['maximum', 'product']: defaultGroupingComposition2d(kernel_size, stride, padding, grouping_big=grouping_big, grouping_list=grouping_list),
+
+        'grouping_comb_prodAndOB': lambda kernel_size, stride=None, padding=0, grouping_list=['product', 'ob']: defaultGroupingComb2d(kernel_size, stride, padding, grouping_list=grouping_list),
+        'grouping_comb_maxAndProd': lambda kernel_size, stride=None, padding=0, grouping_list=['maximum', 'product']: defaultGroupingComb2d(kernel_size, stride, padding, grouping_list=grouping_list),
+        'grouping_comb_maxAndOB': lambda kernel_size, stride=None, padding=0, grouping_list=['maximum', 'ob']: defaultGroupingComb2d(kernel_size, stride, padding, grouping_list=grouping_list),
+        'grouping_comb_maxProdAndOB': lambda kernel_size, stride=None, padding=0, grouping_list=['maximum', 'product', 'ob']: defaultGroupingComb2d(kernel_size, stride, padding, grouping_list=grouping_list)
+
+        #'overlap_product': lambda kernel_size, stride=None, padding=0, overlap='product': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
+        #'overlap_minimum': lambda kernel_size, stride=None, padding=0, overlap='minimum': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
+        #'overlap_ob': lambda kernel_size, stride=None, padding=0, overlap='ob': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap),
+        #'overlap_geometric': lambda kernel_size, stride=None, padding=0, overlap='geometric': defaultOverlap2d(kernel_size, stride, padding, overlap=overlap)
     }
 
     return available_options[pool_option]
