f3c378908e063e359864aa4f40d57ff7565eab55
iosurodri <iosu-rodri@hotmail.com>
2021-12-28 13:46:10 +0100

    REFACT: visualize functions now create a new figure beforehand

--- src/visualization/visualize_distributions.py
+++ src/visualization/visualize_distributions.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 
 
 def visualize_heatmap(x):
+    plt.figure()
     plt.imshow(x.cpu().detach().numpy(), cmap='hot', interpolation='nearest')
     plt.show()
 
 
 def visualize_hist(x, num_bins=100):
+    plt.figure()
     plt.hist(x.cpu().detach().numpy().reshape(-1), bins=num_bins)
     plt.show()
