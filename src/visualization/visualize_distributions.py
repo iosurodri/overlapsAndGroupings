5c5c8960b9aeab0e621e7b453040c351da2b5ebb
iosurodri <iosu.rodriguez@unavarra.es>
2022-01-04 13:04:46 +0100

    REFACT: Added parameter "title" to both visualize_heatmap() and visualize_dist()

--- src/visualization/visualize_distributions.py
+++ src/visualization/visualize_distributions.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def visualize_heatmap(x):
+def visualize_heatmap(x, title=None):
     plt.figure()
     plt.imshow(x.cpu().detach().numpy(), cmap='hot', interpolation='nearest')
+    if title is not None:
+        plt.title(title)
     plt.show()
 
 
-def visualize_hist(x, num_bins=100):
+def visualize_hist(x, num_bins=100, title=None):
     plt.figure()
     plt.hist(x.cpu().detach().numpy().reshape(-1), bins=num_bins)
+    if title is not None:
+        plt.title(title)
     plt.show()
