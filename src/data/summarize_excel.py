f538e43c2e3d2b2fcf17b6c2a490792dbd6c3962
iosurodri <iosu.rodriguez@unavarra.es>
2022-04-26 10:24:51 +0200

    REFACT: Summarize_experimetns can now read all metrics from results files

--- src/data/summarize_excel.py
+++ src/data/summarize_excel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import argparse
 import os
 import sys
@@ -11,38 +12,71 @@ def parse_args():
     # Prepare argument parser:
     CLI = argparse.ArgumentParser()
     CLI.add_argument("experiment_folder", nargs=1, type=str, help='Name of the folder which contains the given tests')
+    CLI.add_argument("--metric", nargs=1, default="all", choices=['accuracy', 'precision', 'recall', 'f1_score', 'all'])
     return CLI.parse_args()
 
 
-def summarize_experiments(parent_folder):
+def summarize_experiments(parent_folder, metric='all'):
 
-    results_tests = {}
+    if metric == 'all':
+        metrics = ['accuracy', 'precision', 'recall', 'f1_score']
+    else:
+        metrics = [metric]
+    df_metrics = {}
 
-    parent_root = os.path.join(PATH_RESULTS, parent_folder)
-    try:
-        # experiments_folders = sorted(os.listdir(parent_root))
-        experiments_folders = filter(lambda x: '.xlsx' not in x, sorted(os.listdir(parent_root)))
-        for experiment_folder in experiments_folders:
-            experiment_root = os.path.join(parent_root, experiment_folder)
-            test_folders = sorted(os.listdir(experiment_root))
-            results_tests[experiment_folder] = {}
-            for test_folder in test_folders:
-                test_root = os.path.join(experiment_root, test_folder)
-                test_file_root = os.path.join(test_root, 'test_metrics.txt')
-                with open(test_file_root, 'r') as test_file:
-                    first_line = test_file.readline()
-                    result_test = float(re.findall("\d+\.\d+", first_line)[0])
-                    results_tests[experiment_folder][test_folder] = result_test
-            results_tests
-    except FileNotFoundError as e:
-        raise e
-    df_results = pd.DataFrame(results_tests)
-    df_results = df_results.transpose()
-    df_results['mean_acc'] = df_results.mean(axis=1)
-    df_results['std_acc'] = df_results.std(axis=1)
-    with pd.ExcelWriter(os.path.join(parent_root, 'acc_summary.xlsx')) as writer:
-        df_results.to_excel(writer)
+    for metric in metrics:
 
+        results_tests = {}
+
+        parent_root = os.path.join(PATH_RESULTS, parent_folder)
+        try:
+            # experiments_folders = sorted(os.listdir(parent_root))
+            experiments_folders = filter(lambda x: '.xlsx' not in x, sorted(os.listdir(parent_root)))
+            for experiment_folder in experiments_folders:
+                experiment_root = os.path.join(parent_root, experiment_folder)
+                test_folders = sorted(os.listdir(experiment_root))
+                results_tests[experiment_folder] = {}
+                for test_folder in test_folders:
+                    test_root = os.path.join(experiment_root, test_folder)
+                    test_file_root = os.path.join(test_root, 'test_metrics.txt')
+                    with open(test_file_root, 'r') as test_file:
+                        # For 'accuracy' we just read a single value:
+                        if metric == 'accuracy':
+                            first_line = test_file.readline()
+                            metric_val = float(re.findall("\d+\.\d+", first_line)[0])
+                            results_tests[experiment_folder][test_folder] = metric_val
+                        # The remaining metrics are given in format one-vs-all (as many values as classes)
+                        else:
+                            metric_by_class = []
+                            line = test_file.readline()
+                            # Find start of metric description:
+                            while not line.startswith(metric):
+                                line = test_file.readline()
+                            # Iterate over lines containing results for the given metric:
+                            while ']' not in line:
+                                result_strings = re.findall("\d+\.\d+", line)
+                                for result_string in result_strings:
+                                    metric_by_class.append(float(result_string))
+                                line = test_file.readline()
+                            # Read the last line:
+                            result_strings = re.findall("\d+\.\d+", line)
+                            for result_string in result_strings:
+                                metric_by_class.append(float(result_string))
+
+                            # Compute mean metric (using macro-averages for the metrics):
+                            metric_val = np.mean(metric_by_class)
+                            results_tests[experiment_folder][test_folder] = metric_val
+        except FileNotFoundError as e:
+            raise e
+        df_results = pd.DataFrame(results_tests)
+        df_results = df_results.transpose()
+        df_results['mean_{}'.format(metric)] = df_results.mean(axis=1)
+        df_results['std_{}'.format(metric)] = df_results.std(axis=1)
+        df_metrics[metric] = df_results
+    
+    with pd.ExcelWriter(os.path.join(parent_root, 'metric_summary.xlsx')) as writer:    
+        for metric in df_metrics.keys():
+            df_metrics[metric].to_excel(writer, sheet_name=metric)
 
 
 if __name__ == '__main__':
@@ -53,6 +87,7 @@ if __name__ == '__main__':
         sys.argv = [sys.argv[0], *sys.argv[1].split()]
     args = parse_args()
     experiment_folder = args.experiment_folder
+    metric = args.metric
     print('Starting data dumping process')
-    summarize_experiments(experiment_folder[0])
+    summarize_experiments(experiment_folder[0], metric[0])
     print('Excel file successfully written')
