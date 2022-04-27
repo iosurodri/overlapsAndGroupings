cb484ddf0c05a08d2878f9ef28896b200f4b49bb
iosurodri <iosu.rodriguez@unavarra.es>
2022-04-27 09:47:17 +0200

    REFACT: Older version of PyTorch expected PIL Image as input for RandomHorizontalFlip

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -16,8 +16,7 @@ datasets_info = {
         'dataset': datasets.CIFAR10,
         'train_transform': transforms.Compose(
             [transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-            transforms.RandomHorizontalFlip()]
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]
         ),
         'test_transform': transforms.Compose(
             [transforms.ToTensor(),
