d89bf1e95629cbae6d35c0f1a577f9a2406d7267
iosurodri <iosu.rodriguez@unavarra.es>
2022-05-06 14:50:52 +0200

    NEW & TODO: Fixing implementation of VGG for CIFAR10 (lacks BatchNorm)

--- src/models/VGG.py
+++ src/models/VGG.py
@@ -11,6 +11,37 @@ __all__ = [
 ]
 
 
+class VGG_small(nn.Module):
+    def __init__(self, features: nn.Module, num_classes: int = 10, init_weights: bool = True, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.features = features
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(p=dropout),
+            nn.Linear(512, num_classes),
+        )
+        if init_weights:
+            for m in self.modules():
+                if isinstance(m, nn.Conv2d):
+                    nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                    if m.bias is not None:
+                        nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.BatchNorm2d):
+                    nn.init.constant_(m.weight, 1)
+                    nn.init.constant_(m.bias, 0)
+                elif isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, 0, 0.01)
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.flatten(x) # x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
 class VGG(nn.Module):
     def __init__(self, features: nn.Module, num_classes: int = 10, init_weights: bool = True, dropout: float = 0.5) -> None:
         super().__init__()
@@ -75,9 +106,11 @@ cfgs: Dict[str, List[Union[str, int]]] = {
 }
 
 
-def _vgg(cfg: str, batch_norm: bool, pool_layer: nn.Module = None, **kwargs: Any) -> VGG:
-    
-    model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, pool_layer=pool_layer), **kwargs)
+def _vgg(cfg: str, batch_norm: bool, pool_layer: nn.Module = None, small=False, **kwargs: Any) -> VGG:
+    if small:
+        model = VGG_small(make_layers(cfgs[cfg], batch_norm=batch_norm, pool_layer=pool_layer), **kwargs)
+    else:
+        model = VGG(make_layers(cfgs[cfg], batch_norm=batch_norm, pool_layer=pool_layer), **kwargs)
     return model
 
 
@@ -101,3 +134,10 @@ def vgg16_bn(pool_layer: nn.Module = None, **kwargs: Any) -> VGG:
         progress (bool): If True, displays a progress bar of the download to stderr
     """
     return _vgg("D", batch_norm=True, pool_layer=pool_layer, **kwargs)
+
+
+def vgg16_small(pool_layer: nn.Module = None, **kwargs: Any) -> VGG_small:
+    return _vgg("D", batch_norm=False, pool_layer=pool_layer, small=True, **kwargs)
+
+def vgg16_bn_small(pool_layer: nn.Module = None, **kwargs: Any) -> VGG_small:
+    return _vgg("D", batch_norm=True, pool_layer=pool_layer, small=True, **kwargs)
