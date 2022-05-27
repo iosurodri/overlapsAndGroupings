4ead91a46231e75d935e7e67ca3b6dde4024ef94
iosurodri <iosu.rodriguez@unavarra.es>
2022-05-27 14:44:59 +0200

    REFACT: Refactored parameter log generation

--- src/data/log_generation.py
+++ src/data/log_generation.py
@@ -3,59 +3,57 @@
 import torch.nn as nn
 # from layers.pooling_layers import GroupingCombPool2d, GroupingPlusPool2d
 
-def log_distributions(model, writer, iter_number, log_custom_param_dist=False, log_conv_dist=False, log_grad_dist=False,
+def log_distribution(writer, param, iter_number, param_type_name, parameter_idx, log_grad_dist=False):
+    if hasattr(param, 'weight') and param.weight is not None:
+        parameter = param.weight.cpu().detach().numpy().squeeze()
+        if parameter.size == 1:
+            writer.add_scalar('{}{}_weight'.format(param_type_name, parameter_idx), parameter.item(), iter_number)
+        else:
+            writer.add_histogram('{}{}_weight'.format(param_type_name, parameter_idx), parameter, iter_number)
+        if log_grad_dist:
+            parameter = param.weight.grad.cpu().detach().numpy().squeeze()
+            if parameter.size == 1:
+                writer.add_scalar('{}{}_weight_grad'.format(param_type_name, parameter_idx), parameter.item(), iter_number)
+            else:
+                writer.add_histogram('{}{}_weight_grad'.format(param_type_name, parameter_idx), parameter, iter_number)
+    if hasattr(param, 'bias') and param.bias is not None:
+        parameter = param.bias.cpu().detach().numpy().squeeze()
+        if parameter.size == 1:
+            writer.add_scalar('{}{}_bias'.format(param_type_name, parameter_idx), parameter.item(), iter_number)
+        else:
+            writer.add_histogram('{}{}_bias'.format(param_type_name, parameter_idx), parameter, iter_number)
+        if log_grad_dist:
+            parameter = param.bias.grad.cpu().detach().numpy().squeeze()
+            if parameter.size == 1:
+                writer.add_scalar('{}{}_bias_grad'.format(param_type_name, parameter_idx), parameter.item(), iter_number)
+            else:
+                writer.add_histogram('{}{}_bias_grad'.format(param_type_name, parameter_idx), parameter, iter_number)
+
+
+def log_distributions(model, writer, iter_number, log_custom_param_dist=False, log_conv_dist=True, log_linear_dist=True, log_grad_dist=False,
     custom_modules=None, custom_module_name='custom_module'):
 
     # if custom_modules is None:
     #     custom_modules = [GroupingPlusPool2d, GroupingCombPool2d]
     
-
-    if log_custom_param_dist:
-        custom_module_idx = 0
-        for param in model.modules():
-            if type(param) in custom_modules: 
-                if param.weight is not None:
-                    parameter = param.weight.cpu().detach().numpy().squeeze()
-                    if parameter.size == 1:
-                        writer.add_scalar('{}{}_weight'.format(custom_module_name, custom_module_idx), parameter.item(), iter_number)
-                    else:
-                        writer.add_histogram('{}{}_weight'.format(custom_module_name, custom_module_idx), parameter, iter_number)
-                custom_module_idx += 1
-    if log_conv_dist:
-        conv_idx = 0
-        for param in model.modules():
-            if type(param) == nn.Conv2d:  
-                weight = param.weight.cpu().detach().numpy().squeeze()
-                writer.add_histogram('conv{}_weight'.format(conv_idx), weight, iter_number)
-                if param.bias is not None:
-                    bias = param.bias.cpu().detach().numpy().squeeze()
-                    writer.add_histogram('conv{}_bias'.format(conv_idx), bias, iter_number)
-                conv_idx += 1
-
-    if log_grad_dist:
-
+    custom_module_idx = 0
+    conv_idx = 0
+    linear_idx = 0
+    for param in model.modules():
         if log_custom_param_dist:
-            custom_module_idx = 0
-            for param in model.modules():
-                if type(param) in custom_modules: 
-                    if param.weight is not None:
-                        parameter = param.weight.grad.cpu().detach().numpy().squeeze()
-                        if parameter.size == 1:
-                            writer.add_scalar('{}{}_weight_grad'.format(custom_module_name, custom_module_idx), parameter.item(), iter_number)
-                        else:
-                            writer.add_histogram('{}{}_weight_grad'.format(custom_module_name, custom_module_idx), parameter, iter_number)
-                    custom_module_idx += 1
-
-        conv_idx = 0
-        for param in model.modules():
+            if type(param) in custom_modules: 
+                log_distribution(writer, param, iter_number, custom_module_name, custom_module_idx, log_grad_dist)
+                custom_module_idx += 1
+        if log_conv_dist:
             if type(param) == nn.Conv2d:
-                weight_grad = param.weight.grad.cpu().detach().numpy().squeeze()
-                writer.add_histogram('conv{}_weight_grad'.format(conv_idx), weight_grad, iter_number)
-                if param.bias is not None:
-                    bias_grad = param.bias.grad.cpu().detach().numpy().squeeze()
-                    writer.add_histogram('conv{}_bias_grad'.format(conv_idx), bias_grad, iter_number)
+                log_distribution(writer, param, iter_number, 'conv', conv_idx, log_grad_dist) 
                 conv_idx += 1
+        if log_linear_dist:
+            if type(param) == nn.Linear:
+                log_distribution(writer, param, iter_number, 'linear', linear_idx, log_grad_dist)
+                linear_idx += 1
 
+   
 def log_activations(model, writer, sample_batch, iter_number):
 
     wrapping_modules = [
