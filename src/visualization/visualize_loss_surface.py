NEW & TODO: Adding filter normalization to random directions. Also fixed minor bugs.

src/visualization/visualize_loss_surface.py
@@ -5,7 +5,18 @@ import numpy as np
 from src.model_tools.evaluate import evaluate
 import matplotlib.pyplot as plt
 
-def get_params_and_directions(model, excluded_parameter_types=None):
+from tqdm import tqdm
+
+import os
+PATH_REPORTS = os.path.join('..', '..', 'reports')
+
+PATH_PLOTS = os.path.join(PATH_REPORTS, 'plots')
+try:
+    os.mkdir(PATH_PLOTS)
+except FileExistsError as e:
+    pass
+
+def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
     # excluded_parameter_types = [nn.BatchNorm2d]
 
     # Get the parameters of the model:
@@ -13,23 +24,79 @@ def get_params_and_directions(model, excluded_parameter_types=None):
     directions2 = []
     parameters = []
     modules = model.modules()
-    prev_module = next(modules) # Get rid of Linear, since modules is a (consumable) iterable
+    _ = next(modules) # Get rid of Linear, since modules is a (consumable) iterable
     for module in modules:
         if ((type(module) != nn.Sequential)) and (len(module._modules) == 0):# or (type(module) in custom_capsule_module_types):
-            for param in module.parameters():
-                parameters.append(param.clone().detach())
-                if type(module) in excluded_parameter_types:
-                    new_direction1 = param.data.new_zeros(param.data.shape)
-                    new_direction2 = param.data.new_zeros(param.data.shape)
+            if type(module) == torch.nn.Conv2d:
+                '''Filter normalization: 
+                   In order to avoid scale invariance problems, the filters of convolutional layers (and linear layers)
+                   must be normalized so that each filter's random direction has norm equal to its filter's norm.
+                '''
+                weight = module.weight.clone().detach()
+                parameters.append(weight)
+                # Generate random directions for filters of the convolution layer:                    
+                new_direction1_weight = torch.rand(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                new_direction2_weight = torch.rand(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                # Reshape weight filters to vector form:
+                weight = torch.reshape(weight, [weight.shape[0], weight.shape[1] * weight.shape[2] * weight.shape[3]])
+                if module.bias is not None:
+                    ### bias=True: The norm of each filter is the norm of the values of the filter's weight, and its bias:
+                    bias = module.bias.clone().detach()
+                    parameters.append(bias)
+                    # Generate random direction for bias term of the convolution layer:
+                    new_direction1_bias = torch.rand(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    new_direction2_bias = torch.rand(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    # Reshape bias term to be compatible with weigh:
+                    bias = bias.unsqueeze(1)
+                    # Compute the norm of each filter (taking into account the weight values and the bias term):
+                    filter_norm = torch.norm(torch.concat([weight, bias], dim=1), dim=1)
+                    # Reshape the random directions taking into account the term for the weight values and bias term and compute their norms:
+                    new_direction1 = torch.concat([
+                        torch.reshape(new_direction1_weight, weight.shape), new_direction1_bias.unsqueeze(1)
+                    ], dim=1)
+                    new_direction1_norm = torch.norm(new_direction1, dim=1)
+                    new_direction2 = torch.concat([
+                        torch.reshape(new_direction2_weight, weight.shape), new_direction2_bias.unsqueeze(1)
+                    ], dim=1)
+                    new_direction2_norm = torch.norm(new_direction2, dim=1)
+                    # Normalize all the random directions to have norm equal to the norm of the filter:
+                    directions1.append((new_direction1_weight / new_direction1_norm[:, None, None, None]) * filter_norm[:, None, None, None])
+                    directions2.append((new_direction2_weight / new_direction2_norm[:, None, None, None]) * filter_norm[:, None, None, None])
+                    directions1.append((new_direction1_bias / new_direction1_norm) * filter_norm)
+                    directions2.append((new_direction2_bias / new_direction2_norm) * filter_norm)
                 else:
-                    new_direction1 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
-                    new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
-                    # while torch.abs(torch.cosine_similarity(new_direction1, new_direction2)) < 0.5:
-                        # Regenerate the second random direction (we want them to be different)
-                        # new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
-                    # TODO: Filterwise normalization:
-                directions1.append(new_direction1)  # We could also append something like [type(param), new_direction1]
-                directions2.append(new_direction2)
+                    ### bias=False: If there is no bias, then the norm of each filter is only affected by its weight:
+                    filter_norm = torch.norm(weight, dim=1)
+                    new_direction1_norm = torch.norm(torch.reshape(new_direction1_weight, weight.shape), dim=1)
+                    new_direction2_norm = torch.norm(torch.reshape(new_direction2_weight, weight.shape), dim=1)
+                    # Filter normalize the random directions:
+                    directions1.append((new_direction1_weight / new_direction1_norm[:, None, None, None]) * filter_norm[:, None, None, None])  # new_direction1_norm[:, None, None, None] acts as a "broadcastable operation":
+                    directions2.append((new_direction2_weight / new_direction1_norm[:, None, None, None]) * filter_norm[:, None, None, None])
+            elif type(module) == torch.nn.Linear:
+                # TODO: Implement
+                pass
+            else:
+                for param in module.parameters():
+                    if param is not None:
+                        parameters.append(param.clone().detach())
+                        if type(module) in excluded_parameter_types:
+                            new_direction1 = param.data.new_zeros(param.data.shape)
+                            new_direction2 = param.data.new_zeros(param.data.shape)
+                        else:
+                            new_direction1 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
+                            new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
+                            # while torch.abs(torch.cosine_similarity(new_direction1, new_direction2)) < 0.5:
+                                # Regenerate the second random direction (we want them to be different)
+                                # new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
+                            # TODO: Filterwise normalization:
+                            if (type(module) == nn.Conv2d) and (param.dim() == 4):
+                                new_direction1 = (new_direction1 / torch.norm(new_direction1, dim=0, keepdim=True)) * torch.norm(param.data, dim=0, keepdim=True)
+                                new_direction2 = (new_direction2 / torch.norm(new_direction2, dim=0, keepdim=True)) * torch.norm(param.data, dim=0, keepdim=True)
+                            elif type(module) == nn.Linear:
+                                new_direction1 = (new_direction1 / torch.norm(new_direction1, dim=0, keepdim=True)) * torch.norm(param.data, dim=0, keepdim=True)
+                                new_direction2 = (new_direction2 / torch.norm(new_direction2, dim=0, keepdim=True)) * torch.norm(param.data, dim=0, keepdim=True)
+                        directions1.append(new_direction1)  # We could also append something like [type(param), new_direction1]
+                        directions2.append(new_direction2)
     return parameters, directions1, directions2
 
 
@@ -41,9 +108,10 @@ def set_model_parameters(model, parameters, directions1, directions2, alpha=0, b
         for module in modules:
             if ((type(module) != nn.Sequential)) and (len(module._modules) == 0):# or (type(module) in custom_capsule_module_types):
                 for param in module.parameters():
-                    new_value = parameters[param_idx] + alpha * directions1[param_idx] + beta * directions2[param_idx]
-                    param.copy_(new_value)
-                    param_idx += 1
+                    if param is not None:  # For torch.nn.Conv2d or torch.nn.Linear, if bias=False, param can be None
+                        new_value = parameters[param_idx] + alpha * directions1[param_idx] + beta * directions2[param_idx]
+                        param.copy_(new_value)
+                        param_idx += 1
     return model
 
 
@@ -51,10 +119,10 @@ def evaluate_loss_surface(model, parameters, directions1, directions2, test_load
 
     acc_surface = []
     loss_surface = []
-    for alpha in alphas:
+    for alpha in tqdm(alphas, unit='alphas'):
         acc_row = []
         loss_row = []
-        for beta in betas:
+        for beta in tqdm(betas, unit='betas'):
             model = set_model_parameters(model, parameters, directions1, directions2, alpha=alpha, beta=beta)
             accuracy, loss = evaluate(model, criterion, test_loader=test_loader)
             acc_row.append(accuracy)
@@ -64,7 +132,7 @@ def evaluate_loss_surface(model, parameters, directions1, directions2, test_load
     return loss_surface, acc_surface
 
 
-def visualize_loss_surface(model, test_loader, criterion, excluded_parameter_types=None):
+def visualize_loss_surface(model, test_loader, criterion=torch.nn.CrossEntropyLoss(), excluded_parameter_types=[torch.nn.BatchNorm2d]):
 
     alphas = np.arange(-1, 1, 0.1)
     betas = np.arange(-1, 1, 0.1)
@@ -84,10 +152,7 @@ def visualize_loss_surface(model, test_loader, criterion, excluded_parameter_typ
     ax.set_xlabel('\u03B1')
     ax.set_ylabel('\u03B2')
     plt.show()
-
-
-
-
+    fig.savefig(os.path.join(PATH_PLOTS, 'contour_plot.pdf'), bbox_inches="tight")
 
 
     # # custom_capsule_module_types: Includes a list of types which encapsulate parameterized modules. This information