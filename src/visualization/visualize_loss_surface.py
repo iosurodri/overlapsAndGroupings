34027417d497a20c7b34a183b5fdb35bd3d6e447
iosurodri <iosu.rodriguez@unavarra.es>
2022-06-30 11:41:40 +0200

    FIX: Surface visualization is (should be) correct now.

--- src/visualization/visualize_loss_surface.py
+++ src/visualization/visualize_loss_surface.py
@@ -17,6 +17,25 @@ except FileExistsError as e:
     pass
 
 def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
+    """Extracts a copy of the parameters of a given model and generates two random (filter normalized) direction vectors in the feature space:
+    If the features of a feature model are seen as a vector F in a dimensional space (defined by all the parameters of all layers of the model),
+    two random vectors in the same dimensional space d1 and d2 are sampled from a gaussian distribution. 
+    Note: In order to avoid scale invariance issues, the parameters of d1 and d2 corresponding to convolutional or linear layers are normalized
+    based on their corresponding filter norms. That is, roughly speaking: d1_i <- (d1_i / ||d1_i||) * ||F_i||
+    More info (source): Visualizing the Loss Landscape of Neural Nets (https://arxiv.org/abs/1712.09913)
+
+    Args:
+        model (modelType): Model from which parameters are to be copied.
+        excluded_parameter_types (list<torch.nn.Module>, optional): List of torch.nn.Module types for which directions are to be ignored. 
+            In the original paper, Batch Normalization layer parameters are not considered in d1 and d2 (vectors of zeros are generated for these parameters).
+            Other user defined torch.nn.Modules can be included as well.
+
+    Returns:
+        list<torch.nn.Tensor>: A copy of all the parameters in "model", returned in the order presented by model.parameters()
+        list<torch.nn.Tensor>: First random direction
+        list<torch.nn.Tensor>: Second random direction
+    """
+
     # excluded_parameter_types = [nn.BatchNorm2d]
 
     # Get the parameters of the model:
@@ -35,8 +54,8 @@ def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
                 weight = module.weight.clone().detach()
                 parameters.append(weight)
                 # Generate random directions for filters of the convolution layer:                    
-                new_direction1_weight = torch.rand(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
-                new_direction2_weight = torch.rand(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                new_direction1_weight = torch.randn(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                new_direction2_weight = torch.randn(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
                 # Reshape weight filters to vector form:
                 weight = torch.reshape(weight, [weight.shape[0], weight.shape[1] * weight.shape[2] * weight.shape[3]])
                 if module.bias is not None:
@@ -44,8 +63,8 @@ def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
                     bias = module.bias.clone().detach()
                     parameters.append(bias)
                     # Generate random direction for bias term of the convolution layer:
-                    new_direction1_bias = torch.rand(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
-                    new_direction2_bias = torch.rand(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    new_direction1_bias = torch.randn(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    new_direction2_bias = torch.randn(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
                     # Reshape bias term to be compatible with weigh:
                     bias = bias.unsqueeze(1)
                     # Compute the norm of each filter (taking into account the weight values and the bias term):
@@ -74,7 +93,40 @@ def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
                     directions2.append((new_direction2_weight / new_direction1_norm[:, None, None, None]) * filter_norm[:, None, None, None])
             elif type(module) == torch.nn.Linear:
                 # TODO: Implement
-                pass
+                weight = module.weight.clone().detach()
+                parameters.append(weight)
+                # Generate random directions for filters of the convolution layer:                    
+                new_direction1_weight = torch.randn(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                new_direction2_weight = torch.randn(weight.data.shape, device=weight.data.device, dtype=weight.data.dtype)
+                if module.bias is not None:
+                    ### bias=True: The norm of each filter is the norm of the values of the filter's weight, and its bias:
+                    bias = module.bias.clone().detach()
+                    parameters.append(bias)
+                    # Generate random direction for bias term of the convolution layer:
+                    new_direction1_bias = torch.randn(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    new_direction2_bias = torch.randn(bias.data.shape, device=bias.data.device, dtype=bias.data.dtype)
+                    # Reshape bias term to be compatible with weigh:
+                    bias = bias.unsqueeze(1)
+                    # Compute the norm of each filter (taking into account the weight values and the bias term):
+                    filter_norm = torch.norm(torch.concat([weight, bias], dim=1), dim=1)
+                    # Reshape the random directions taking into account the term for the weight values and bias term and compute their norms:
+                    new_direction1 = torch.concat([new_direction1_weight, new_direction1_bias.unsqueeze(1)], dim=1)
+                    new_direction1_norm = torch.norm(new_direction1, dim=1)
+                    new_direction2 = torch.concat([new_direction2_weight, new_direction2_bias.unsqueeze(1)], dim=1)
+                    new_direction2_norm = torch.norm(new_direction2, dim=1)
+                    # Normalize all the random directions to have norm equal to the norm of the filter:
+                    directions1.append((new_direction1_weight / new_direction1_norm[:, None]) * filter_norm[:, None])
+                    directions2.append((new_direction2_weight / new_direction2_norm[:, None]) * filter_norm[:, None])
+                    directions1.append((new_direction1_bias / new_direction1_norm) * filter_norm)
+                    directions2.append((new_direction2_bias / new_direction2_norm) * filter_norm)
+                else:
+                    ### bias=False: If there is no bias, then the norm of each filter is only affected by its weight:
+                    filter_norm = torch.norm(weight, dim=1)
+                    new_direction1_norm = torch.norm(new_direction1_weight, dim=1)
+                    new_direction2_norm = torch.norm(new_direction2_weight, dim=1)
+                    # Filter normalize the random directions:
+                    directions1.append((new_direction1_weight / new_direction1_norm[:, None]) * filter_norm[:, None])  # new_direction1_norm[:, None, None, None] acts as a "broadcastable operation":
+                    directions2.append((new_direction2_weight / new_direction1_norm[:, None]) * filter_norm[:, None])
             else:
                 for param in module.parameters():
                     if param is not None:
@@ -83,8 +135,8 @@ def get_params_and_directions(model, excluded_parameter_types=[nn.BatchNorm2d]):
                             new_direction1 = param.data.new_zeros(param.data.shape)
                             new_direction2 = param.data.new_zeros(param.data.shape)
                         else:
-                            new_direction1 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
-                            new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
+                            new_direction1 = torch.randn(param.data.shape, device=param.data.device, dtype=param.data.dtype)
+                            new_direction2 = torch.randn(param.data.shape, device=param.data.device, dtype=param.data.dtype)
                             # while torch.abs(torch.cosine_similarity(new_direction1, new_direction2)) < 0.5:
                                 # Regenerate the second random direction (we want them to be different)
                                 # new_direction2 = torch.rand(param.data.shape, device=param.data.device, dtype=param.data.dtype)
@@ -116,7 +168,22 @@ def set_model_parameters(model, parameters, directions1, directions2, alpha=0, b
 
 
 def evaluate_loss_surface(model, parameters, directions1, directions2, test_loader, criterion, alphas=np.arange(-1, 1, 0.1), betas=np.arange(-1, 1, 0.1)):
+    """Generate numpy arrays representing the cost (and accuracy) surface of a given model, evaluated with parameters:
+    model.features = features + (alphas_i * directions1) + (betas_j * directions2)
 
+    Args:
+        model (modelType): Model to be evaluated with different parameter configurations.
+        parameters (_type_): _description_
+        directions1 (_type_): _description_
+        directions2 (_type_): _description_
+        test_loader (_type_): _description_
+        criterion (_type_): _description_
+        alphas (_type_, optional): _description_. Defaults to np.arange(-1, 1, 0.1).
+        betas (_type_, optional): _description_. Defaults to np.arange(-1, 1, 0.1).
+
+    Returns:
+        _type_: _description_
+    """
     acc_surface = []
     loss_surface = []
     for alpha in tqdm(alphas, unit='alphas'):
@@ -129,79 +196,44 @@ def evaluate_loss_surface(model, parameters, directions1, directions2, test_load
             loss_row.append(loss)
         acc_surface.append(acc_row)
         loss_surface.append(loss_row)
-    return loss_surface, acc_surface
+    return np.array(loss_surface), np.array(acc_surface)
 
 
-def visualize_loss_surface(model, test_loader, criterion=torch.nn.CrossEntropyLoss(), excluded_parameter_types=[torch.nn.BatchNorm2d]):
+def visualize_loss_surface(model, test_loader, name, criterion=torch.nn.CrossEntropyLoss(), excluded_parameter_types=[torch.nn.BatchNorm2d]):
 
     alphas = np.arange(-1, 1, 0.1)
     betas = np.arange(-1, 1, 0.1)
 
     parameters, directions1, directions2 = get_params_and_directions(model, excluded_parameter_types)
-    loss_surface, acc_surface = evaluate_loss_surface(model, parameters, directions1, directions2, test_loader, criterion, alphas=alphas, betas=betas)
+    loss_surface_np, acc_surface_np = evaluate_loss_surface(model, parameters, directions1, directions2, test_loader, criterion, alphas=alphas, betas=betas)
+    # Save loss_surface and acc_surface as csv files:
+    np.savetext(os.path.join(PATH_PLOTS, 'loss_surface_{}.csv'.format(name)), loss_surface_np, delimiter=",")
+    np.savetext(os.path.join(PATH_PLOTS, 'acc_surface_{}.csv'.format(name)), acc_surface_np, delimiter=",")
 
-    # Generate visualization using PyPlot   
+    # Plot loss surface:
     fig = plt.figure(figsize=(6,5))
     left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
     ax = fig.add_axes([left, bottom, width, height]) 
 
-    loss_surface_np = np.array(loss_surface)
     cp = ax.contour(alphas, betas, loss_surface_np)
     ax.clabel(cp, inline=True, fontsize=10)
-    ax.set_title('Contour Plot')
+    ax.set_title('Contour Plot (Loss)')
     ax.set_xlabel('\u03B1')
     ax.set_ylabel('\u03B2')
     plt.show()
-    fig.savefig(os.path.join(PATH_PLOTS, 'contour_plot.pdf'), bbox_inches="tight")
+    fig.savefig(os.path.join(PATH_PLOTS, 'loss_surface_{}.pdf'.format(name)), bbox_inches="tight")
+    fig.savefig(os.path.join(PATH_PLOTS, 'loss_surface_{}.png'.format(name)), bbox_inches="tight")
 
+    # Plot accuracy surface:
+    fig = plt.figure(figsize=(6,5))
+    left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+    ax = fig.add_axes([left, bottom, width, height]) 
 
-    # # custom_capsule_module_types: Includes a list of types which encapsulate parameterized modules. This information
-    # #   is used for making sure that the parameters of the child module are not stored twice.
-
-    # center_point = []
-    
-    # # Get the parameters of the model:
-    # modules = model.modules()
-    # prev_module = next(modules) # Get rid of Linear, since modules is a (consumable) iterable
-    # for module in modules:
-    #     if ((type(module) != nn.Sequential)) and (len(module._modules) == 0) or (type(module) in custom_capsule_module_types):
-    #         if type(module) in 
-
-
-    # common_modules = nn.ModuleList()
-    # custom_modules = nn.ModuleList()
-    # prev_module = None
-    # module = next(modules)
-    # for module in modules:
-    #     # TODO: len(module._modules) should indicate if module contains other modules, but there can exist edge cases
-    #     if ((type(module) != nn.Sequential) and (len(module._modules) == 0) or (type(module) in custom_capsule_module_types)) and (type(prev_module) not in custom_capsule_module_types):
-    #         if type(module) in custom_module_types:
-    #             if hasattr(module, 'parameters'):
-    #                 custom_modules.append(module)
-    #         else:
-    #             if hasattr(module, 'parameters'):
-    #                 common_modules.append(module)
-    #     prev_module = module
-    # common_parameters = common_modules.parameters()
-    # custom_parameters = custom_modules.parameters()
-    # # TODO: Add condition to check that all parameters are present (just once) either in custom_modules or common_modules
-    # # if completeness_check:
-    #     # This condition would be slow, so it should only be used with debugging purposes
-    # if completeness_check:
-    #     common_parameter_list = list(common_parameters)
-    #     custom_parameter_list = list(custom_parameters)
-    #     for param in model.parameters():
-    #         param_in_common_param_list = False
-    #         param_in_custom_param_list = False
-    #         i = 0
-    #         while i < len(common_parameter_list) and param is not common_parameter_list[i]:
-    #             i += 1
-    #         param_in_common_param_list = i < len(common_parameter_list)
-    #         i = 0
-    #         while i < len(custom_parameter_list) and param is not custom_parameter_list[i]:
-    #             i += 1
-    #         param_in_custom_param_list = i < len(custom_parameter_list)
-    #         if (not param_in_common_param_list) and (not param_in_custom_param_list):
-    #             raise Exception('FAIL: Not all parameters are being optimized')
-    #     raise Exception('SUCCESS: All parameters are being optimized!!')
-    # return common_parameters, custom_parameters
+    cp = ax.contour(alphas, betas, acc_surface_np)
+    ax.clabel(cp, inline=True, fontsize=10)
+    ax.set_title('Contour Plot (Accuracy)')
+    ax.set_xlabel('\u03B1')
+    ax.set_ylabel('\u03B2')
+    plt.show()
+    fig.savefig(os.path.join(PATH_PLOTS, 'acc_surface_{}.pdf'.format(name)), bbox_inches="tight")
+    fig.savefig(os.path.join(PATH_PLOTS, 'acc_surface_{}.pdf'.format(name)), bbox_inches="tight")
